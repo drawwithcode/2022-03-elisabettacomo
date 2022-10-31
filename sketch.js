50c31f4ace30be386f1e2d9930c1989dad562841
elisabettacomo <elisabettacomo@outlook.it>
2022-10-31 14:23:23 +0100

    MY LOVE FINDER
    A virtual machine that associates you face to your perfect soul mate.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-//Codice che funziona fino a quando non ho provato a mettere l’array
-
 //VAR FOR SNAP IMAGE
 var video;
 var snapshot = [];
@@ -18,9 +16,6 @@ let stopRunningDraw = 1;
 
 function preload() {
     bg = loadImage("./assets/background.png");
-    // myTitle = loadImage("./assets/title.png");
-    // myBody = loadImage("./assets/body-back.png");
-    // myFooter = loadImage("./assets/para-back.png");
     mySystem = loadImage("./assets/ingombri.png");
     myRetry = loadImage("./assets/retry.svg");
     mySave = loadImage("./assets/save.svg");
@@ -43,7 +38,7 @@ function setup() {
     video = createCapture(VIDEO); //access live webcam 
 
     button = createImg('./assets/retry.svg');
-    button.position(width/4.8, height/1.4);
+    button.position(width/4.3, height/1.4);
     button.size(92, 85);
     button.mousePressed(restartBin);
 
@@ -56,30 +51,24 @@ function setup() {
     button3.position(width/2.2, height/1.48);
     button3.size(160, 143);
     button3.mousePressed(randomPicker);
-
-    // gif.setFrame(0);
-    // gif.pause();
 }
 
 
 function draw() {
 
     image(bg, width/2, height/2, windowWidth, windowHeight);
-    image(mySystem, width/2, height/2, 1246/1.4, 983/1.4);
-    //image(myBin, width/4.2, height/1.32, 94/1.4, 104/1.4);
-    //image(myFolder, width/1.32, height/1.32, 107/1.4, 94/1.4);
-    image(myPicContainer, width/2.8, height/2.1, 516/1.3, 431/1.3);
-    image(myResultContainer, width/1.55, height/2.1, 516/1.3, 431/1.3);
+    image(mySystem, width/2, height/2, 1246/1.2, 983/1.2);
+    image(myPicContainer, width/2.8, height/2.025, 516/1.2, 431/1.2);
+    image(myResultContainer, width/1.55, height/2.025, 516/1.2, 431/1.2);
 
-    image(video, width/2.82, height/2.07, width/4, width * video.height / video.width / 4); 
+    image(video, width/2.82, height/2, 1001/2.8, 742/2.8); 
 
-    //image(gif, width/1.55, height/2.1, 1001/2.8, 742/2.8);
 
     //draw to the canvas using the random
     //image(image, x position, y position, image width, image height)
     if(stopRunningDraw == 0){
         let randoImg = random(cards);    
-        image(randoImg, width/1.55, height/2.07, 1001/2.8, 742/2.8);
+        image(randoImg, width/1.55, height/2, 1001/2.8, 742/2.8);
     }
     
 }
@@ -100,8 +89,6 @@ function randomPicker() {
             noLoop();
         }, 1000);
         
-        // gif.setFrame(0);
-        // gif.play();
 }
 
 function windowResized(){
