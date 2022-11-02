9a03df61d5d932b8b636248bceab9c8eb841480c
elisabettacomo <elisabettacomo@outlook.it>
2022-11-02 01:38:49 +0100

    MY LOVE FINDER
    A virtual machine that associates you face to your perfect soul mate.

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-//VAR FOR SNAP IMAGE
+// VARIABLES FOR THE CAMERA/SNAP OF THE CAMERA
 var video;
 var snapshot = [];
 
@@ -7,14 +7,16 @@ var button;
 var button2;
 var button3;
 
+//AUDIO EFFECT VAR
 let song;
 
-// VARIABLES FOR RANDOM IMAGE PICKER
+// VARIABLES FOR THE RANDOM IMAGE PICKER
 let cards = []; // CREATE AN EMPTY ARRAY TO STORE VARIABLES
 let numCards = 21;
 
-let stopRunningDraw = 1;
+let stopRunningDraw = 1; //A GENERAL VARIABLE THAT I'M GOING TO USE LATER TO STOP THE RANDOMNESS OF THE ARRAY
 
+//PRELOAD FUNCTION: LOADING ALL MY ASSETS
 function preload() {
     bg = loadImage("./assets/background.png");
     mySystem = loadImage("./assets/ingombri.png");
@@ -26,74 +28,84 @@ function preload() {
 
     song = loadSound("./assets/yay.mp3");
 
-    // gif = loadImage("./assets/gif-anim.gif");
-    // the most efficient way to store images is by using a for loop and concatenation
+    //store images  by using a for loop and concatenation
+    //i rename all my images using the same scheme: pers + 0,1,2... + .png
     for (let i = 0; i < numCards; i++) {
         cards[i] = loadImage("./assets/images/pers" + i + '.png');
     }
   }
   
-
+//setup function to set basic instructions
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    imageMode(CENTER);
+
+    createCanvas(windowWidth, windowHeight); //my sketch occupies the whole page
+
+    imageMode(CENTER);//the pivot of the images is in the center
 
     video = createCapture(VIDEO); //access live webcam 
 
-    button = createImg('./assets/retry.svg');
-    button.position(width/4.3, height/1.42);
-    button.size(100, 93);
-    button.mousePressed(restartBin);
+    //the buttons are going to be linked to some actions
 
-    button2 = createImg('./assets/save.svg');
+    //button 1 restarts the page after mouse click
+    button = createImg('./assets/retry.svg'); //the button is linked to an image
+    button.position(width/4.3, height/1.42); //set the position
+    button.size(100, 93); //set the size
+    button.mousePressed(restartBin); //set the function after the action
+
+    //button 2 saves the canvas
+    button2 = createImg('./assets/save.svg'); 
     button2.position(width/1.4, height/1.42);
     button2.size(100, 93);
     button2.mousePressed(saveFrame);
 
+    //button 3 takes the photo
     button3 = createImg('./assets/snap.svg');
     button3.position(width/2.2, height/1.48);
     button3.size(160, 143);
     button3.mousePressed(randomPicker);
 }
 
-
+//function DRAW to place the elements
 function draw() {
 
-    image(bg, width/2, height/2, windowWidth, windowHeight);
-    image(mySystem, width/2, height/2, 1246/1.2, 983/1.2);
+    image(bg, width/2, height/2, windowWidth, windowHeight); //background
+    image(mySystem, width/2, height/2, 1246/1.2, 983/1.2); //assets
     image(myPicContainer, width/2.8, height/2.025, 516/1.2, 431/1.2);
     image(myResultContainer, width/1.55, height/2.025, 516/1.2, 431/1.2);
 
-    image(video, width/2.82, height/2, 1001/2.8, 742/2.8); 
+    image(video, width/2.82, height/2, 1001/2.8, 742/2.8); //place the video
 
 
-    //draw to the canvas using the random
-    //image(image, x position, y position, image width, image height)
+    //draw the random images
+    //condition: if my general variable "stopRunningDraw" is 0, draw a random image (randoImg) picked from the array "cards"
     if(stopRunningDraw == 0){
-        let randoImg = random(cards);    
-        image(randoImg, width/1.55, height/2, 1001/2.8, 742/2.8);
+        let randoImg = random(cards); //setting the variable "randoImg" that corresponds to the array "cards"
+        image(randoImg, width/1.55, height/2, 1001/2.8, 742/2.8); //draw "randoImg"
     }
     
 }
 
+//function takesnap: where the video stops and returns a still frame
 function takesnap() {
-    snapshot = video.get();
-    print(snapshot);
+    snapshot = video.get(); //call the video
+    print(snapshot); //returns the snapshot
 }
 
+//when "P" is pressed:
 function keyPressed() {
-    if (key === 'p') {
-        takesnap();
-        song.play();
-        stopRunningDraw = 0;
+    if (key === 'p') { 
+        takesnap(); //run the function takesnap
+        song.play(); //play the audio file
+
+        stopRunningDraw = 0; //change the value of "stopRunningDraw" into 0, so that my array starts running
         
-        setTimeout(function(){
-            noLoop();
+        setTimeout(function(){ //give a delay to the action "No loop"
+            noLoop(); //the array stops to a random image
         }, 1000);
     } 
 }
 
-
+//same as when "P" is pressed:
 function randomPicker() {
         takesnap();
         song.play();
@@ -105,14 +117,17 @@ function randomPicker() {
         
 }
 
+//resize canvas when the window changes dimensions
 function windowResized(){
     resizeCanvas(windowWidth, windowHeight);
 }
 
+//saves the canvas under the name "iFoundTheLove.png"
 function saveFrame() {
     save("iFoundTheLove.png");
 }
 
+//reloads the whole page
 function restartBin() {
     window.location.reload(); 
 }
